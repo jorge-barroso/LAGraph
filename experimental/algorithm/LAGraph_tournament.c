e00150c60b3f82f2446286d40d129f9ed70cb858
jorge-barroso <jorge_barroso_11@hotmail.com>
2023-10-16 01:08:00 -0500

    Refactor 'is_tournament' function, add error message return
    The 'is_tournament' function in LAGraph_tournament.c has been refactored. Memory cleanup has been streamlined by removing superfluous GrB_free calls for variables nvals, nrows, and ncols. Moreover, the function's signature has been modified to return an error message on detection failure. This will allow improved debugging and better user feedback. The removal of redundancies improves the overall code efficiency and readability.

--- experimental/algorithm/LAGraph_tournament.c
+++ experimental/algorithm/LAGraph_tournament.c
@@ -4,25 +4,23 @@
 
 #define LG_FREE_WORK                \
 {                                   \
-    GrB_free (&nvals) ;               \
-    GrB_free (&nrows) ;                 \
-    GrB_free (&ncols) ;             \
     GrB_free (&U) ;              \
 }
 
 #include "LG_internal.h"
 
-bool is_tournament(LAGraph_Graph G) {
+bool is_tournament(LAGraph_Graph G,
+                   char *msg) {
     GrB_Matrix A = G->A;
     GrB_Index nvals, nrows, ncols;
+    GrB_Matrix U = NULL;
     GRB_TRY(GrB_Matrix_nvals(&nvals, A));
-    GRB_TRY(GrB_Matrix_nrows(&nrows, A);)
+    GRB_TRY(GrB_Matrix_nrows(&nrows, A));
     GRB_TRY(GrB_Matrix_ncols(&ncols, A));
     if (nvals != nrows * floor((ncols - 1) / 2.0) || G->nself_edges) {
         return false;
     }
 
-    GrB_Matrix U;
     GRB_TRY (GrB_select(U, NULL, NULL, GrB_TRIU, A, (int64_t) 1, NULL));
     GRB_TRY(GrB_Matrix_nvals(&nvals, U));
 
