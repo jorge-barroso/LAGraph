28f725021bab51fb45834f073753e6623118b141
jorge-barroso <jorge_barroso_11@hotmail.com>
2023-11-16 18:36:41 -0600

    Optimize LAGraph_Tournament algorithm implementation
    Revised the LAGraph_Tournament function by declaring and integrating GrB_Matrix 'diag'. This matrix helps to enumerate self loops in the input graph, which enhances validation constraints. Transposition and caching steps are now executed earlier in the function, avoiding redundant computations.

--- experimental/algorithm/LAGraph_Tournament.c
+++ experimental/algorithm/LAGraph_Tournament.c
@@ -5,6 +5,7 @@
 #define LG_FREE_WORK                \
 {                                   \
     GrB_free (&L) ;              \
+    GrB_free (&diag) ;              \
 }
 
 #include "LG_internal.h"
@@ -21,12 +22,20 @@ int LAGraph_IsTournament(
     // Declare items
     GrB_Index nvals, nrows, ncols;
     GrB_Matrix L = NULL;
+    GrB_Matrix diag = NULL;
 
     // Get matrix dimensions
     GRB_TRY(GrB_Matrix_nvals(&nvals, G->A));
     GRB_TRY(GrB_Matrix_nrows(&nrows, G->A));
     GRB_TRY(GrB_Matrix_ncols(&ncols, G->A));
-    LAGRAPH_TRY(LAGraph_Cached_NSelfEdges(G, msg));
+
+    // Transpose and cache
+    GRB_TRY(LAGraph_Cached_AT(G, msg));
+
+//        LAGRAPH_TRY(LAGraph_Cached_NSelfEdges(G, msg));
+    GRB_TRY(GrB_Matrix_new(&diag, GrB_FP64, nrows, ncols));
+    GRB_TRY(GrB_select(diag, G->A, NULL, GrB_DIAG, G->AT, 0, GrB_DESC_S));
+    GRB_TRY(GrB_Matrix_nvals(&G->nself_edges, diag));
 
     // Find whether we can quickly assert that this is not a is_tournament graph
     if (nvals != nrows * (ncols - 1) / 2.0 || G->nself_edges > 0) {
@@ -35,36 +44,10 @@ int LAGraph_IsTournament(
     }
 
     GRB_TRY(GrB_Matrix_new(&L, GrB_FP64, nrows, ncols));
-    GRB_TRY(LAGraph_Cached_AT(G, msg));
-//    Grb_DESC_ST0
     GRB_TRY(GrB_select(L, G->A, NULL, GrB_TRIL, G->AT, -1, GrB_DESC_S));
     GxB_print(L, 3);
     GRB_TRY(GrB_Matrix_nvals(&nvals, L));
     (*is_tournament) = nvals == 0;
-
-// Transpose matrix
-//    GrB_Matrix A_transposed;
-//    GRB_TRY(GrB_Matrix_new(&A_transposed, GrB_UINT64, ncols, nrows));
-//    GRB_TRY(GrB_transpose(A_transposed, GrB_NULL, GrB_NULL, G->A, GrB_NULL));
-//    GxB_print(A_transposed, 3);
-//
-//    // Create selector for upper triangle
-//    GrB_Matrix triu;
-//    GRB_TRY(GrB_Matrix_new(&triu, GrB_UINT64, ncols, nrows));
-//    GRB_TRY(GrB_select(triu, NULL, NULL, GrB_TRIL, A_transposed, -1, NULL));
-//    GxB_print(triu, 3);
-//
-//    // Mask the matrix
-//    GrB_Matrix C;
-//    GRB_TRY(GrB_Matrix_new(&C, GrB_UINT64, ncols, nrows));
-//    GRB_TRY(GrB_mxm(C, G->A, GrB_NULL, GrB_PLUS_TIMES_SEMIRING_UINT64, triu, A_transposed, GrB_NULL));
-//
-//    // Count non-zero values
-//    GrB_Index nvals_after;
-//    GRB_TRY(GrB_Matrix_nvals(&nvals_after, C));
-//    printf("nvals: %lu\n", nvals_after);
-//    printf("\nis tournament 2: %s\n", nvals_after == 0 ? "true" : "false");
-//    (*is_tournament) = nvals_after == 0;
 
     LG_FREE_WORK;
     return (GrB_SUCCESS);
